29e3026b4004975a5d00bfa47b09d9ebaca61a9f
JustinMagnano <jmagnano98@gmail.com>
2025-01-17 20:16:16 -0800

    Delete stockApp directory

--- stockApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-
-admin.site.register(Stock)
-admin.site.register(Portfolio)
-admin.site.register(Transaction)
-admin.site.register(Account)
